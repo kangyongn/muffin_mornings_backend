b3e23463f8950956eb9d52baf7ac06b31c7ac854
github username <github email address>
2019-07-25 11:30:24 -0400

    changed routes to resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :orders, only: [:index, :show, :create, :update, :destroy]
-      resources :muffins, only: [:index, :show, :create, :update, :destroy]
+      resources :orders
+      resources :muffins
     end
   end
 end
